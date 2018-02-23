58b27cba094d307f97e0a2c92804d1dd66254069
based_igor <igor.popelniuk@gmail.com>
2018-02-23 05:32:16 +0200

    red lines 21

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
 
 
 Project.create!(title: 'FIRST PROJECT', description: 'Description is here', link: 'vimeo.com/1488', credits: 'Jesus, mom')
