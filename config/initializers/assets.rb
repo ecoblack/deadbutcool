6f3a601aa0390ae4368b1e123609312435effd36
based_igor <igor.popelniuk@gmail.com>
2018-02-23 02:40:24 +0200

    red lines 10

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,7 +5,12 @@ Rails.application.config.assets.version = '1.0'
 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
-
+Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf|otf|woff2)\z/
+config.assets.paths << Rails.root.join("app", "assets", "fonts")
+    config.assets.precompile << /\.(?:otf|eot|woff|ttf)$/
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
+    config.assets.precompile << /\.(?:otf|eot|woff|ttf)$/
+    Rails.application.config.assets.precompile += %w( '.otf' ) 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
