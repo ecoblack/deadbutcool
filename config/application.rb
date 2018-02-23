accd43983c58816281834b3492e566b755efad65
based_igor <igor.popelniuk@gmail.com>
2018-02-23 02:32:13 +0200

    red lines 8

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,6 @@ module Deadbutcool
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.assets.paths << Rails.root.join("app", "assets", "fonts")
-    config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
+    config.assets.precompile << /\.(?:otf|eot|woff|ttf)$/
   end
 end
