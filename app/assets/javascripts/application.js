97e75305656885db41e332d4aa4008f2a571fd38
based_igor <igor.popelniuk@gmail.com>
2018-03-07 03:25:44 +0200

    test ajax

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,18 +13,108 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+$('img').click(function()
+{
+     $('img').not(this, "#header").each(function()
+     {
+         $(this).animate({opacity: 0}, 1500);
+     });
+     $(this).delay( 1500 ).fadeOut(2000).$('.container').load($(this).attr('href'));
+      //load html from the url and put it in the #content element
+    event.preventDefault(); // 
+    
+})
 
 
-function openNav() {
-    document.getElementById("myNav").style.display = "block";
+
+
+//VIEW PROJECT CASE 
+
+//$(document).ready(function(){
+ //   $(document).ajaxSend(function(event, request, settings) {
+  //      $('#loading-indicator').show();
+  //  });
+//
+   // $(document).ajaxComplete(function(event, request, settings) {
+   //     $('#loading-indicator').hide();
+  //  });
+//});
+
+ $(document).on('click', 'a', function(e) {
+    history.pushState({}, '', $(this).attr('href'));
+  });
+
+  $(window).bind('popstate', function () {
+    $.get(document.location.href)
+  });
+
+
+
+  function historySupport() {
+  return !!(window.history && window.history.pushState !== undefined);
 }
 
-function closeNav() {
-    document.getElementById("myNav").style.display = "none";
+function pushPageState(state, title, href) {
+  if (historySupport()) {
+    history.pushState(state, title, href);
+  }
 }
 
+function replacePageState(state, title, href) {
+  if (state == undefined) { state = null; }
+  
+  if (historySupport()) {
+    history.replaceState(state, title, href);
+  }
+}
 
+$(function() {
+  window.onpopstate = function(e) {
+    if ($('body').attr('data-state-href') === location.href) {
+      return confirm("ONPOPSTATE");
+      return false;
+    }
+    
+    if (e.state !== null) {
+      // do something with your state object
+    }
+    
+    $.ajax({
+     beforeSend:function(){
+     return confirm("Are you sure?");
+},
+      url: location.href,
+      dataType: 'script',
+      success: function(data, status, xhr) {
+       return confirm("SUCCESS");
+      },
+      error: function(xhr, status, error) {
+        // You may want to do something else depending on the stored state
+        alert('Failed to load ' + location.href);
+      },
+      complete: function(xhr, status) {
+        $('body').attr('data-state-href', location.href);
+      }
+    });
+  };
+  
+  
+  var getA = 'a[data-remote][data-method!="get"][data-method!="post"][data-method!="delete"]'
+    , getForm = 'form[data-remote][method="get"]';
+  
+  function getState(el) {
+    // insert your own code here to extract a relevant state object from an <a> or <form> tag
+    // for example, if you rely on any other custom "data-" attributes to determine the link behaviour
+    return {};
+  };
+  
+  $('body').attr('data-state-href', location.href);
+  
+  $(getA).
+    live('ajax:beforeSend', function(xhr) {
+      pushPageState(getState(this), "Loading...", this.href);
+      window.title = "Loading...";
+    });
 
-$ ->
-  $("a[data-remote]").on "ajax:success", (e, data, status, xhr) ->
-    alert "The article was deleted."
+});
+
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,16 +1,29 @@
 class PagesController < ApplicationController
-def index
-	@projects = Project.all
-end
+respond_to :html, :js
+	  def index
+		 @projects = Project.all
+	  end
+	
+	  def info
+	    
+		respond_to do |format|
+	      format.html
+	      format.js
+		end
 
-	def contact
-	@contact = {
-		business_email: 'hello@deadbutcool.com',
-		gen_email: "info@deadbutcool.com",
-		phone: "+7-322-674-88-03",
-		soundcloud: "soundcloud.com/fjordwhore",
-		youtube: "soundcloud.com/fjordwhore",
-		instagram: "soundcloud.com/fjordwhore"}
-		
-	end
+		@contact = {
+			business_email: 'hello@deadbutcool.com',
+			gen_email: "info@deadbutcool.com",
+			phone: "+7-322-674-88-03",
+			soundcloud: "soundcloud.com/fjordwhore",
+			youtube: "soundcloud.com/fjordwhore",
+			instagram: "soundcloud.com/fjordwhore"}
+			
+		end
+		def contact
+			respond_to do |format|
+	      format.html # index.html.erb
+	      format.js
+		end
+		end
 end
